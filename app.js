97542c172da8add261021efc968c54c9af5e003c
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-09 12:32:47 +0500

    JS from book

---
+++ app.js
@@ -0,0 +1,11 @@
+// var spec = prompt("Physics marks", "Out of 100");
+// alert("2 plus 2 equals " + 2 + 2);
+
+
+var math =+prompt("Enter Maths number", "Out of 100");
+var english =+prompt("Enter English number", "Out of 100") ;
+var sci =+prompt("Enter Science number", "Out of 100");
+
+var total= math+english+sci;
+alert(total);
+
