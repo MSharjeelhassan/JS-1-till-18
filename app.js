24aaa3ee1d6cef4017e74bab0d2db3cf16d5bc79
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-09 13:45:05 +0500

    working

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// var spec = prompt("Physics marks", "Out of 100");
-// alert("2 plus 2 equals " + 2 + 2);
-
 
 var math =+prompt("Enter Maths number", "Out of 100");
 var english =+prompt("Enter English number", "Out of 100") ;
@@ -8,4 +5,20 @@ var sci =+prompt("Enter Science number", "Out of 100");
 
 var total= math+english+sci;
 alert(total);
+
+var a=10;
+var b=2;
+var c=a+b;
+console.log(c);
+
+var c=a++;
+console.log(a)
+
+var d=--c;
+console.log(d)
+
+var age= prompt("Enter your age","Must be in numbers");
+var nic =prompt("Do you have cnic","yes")
+
+if (age>=18 || nic==yes) {alert("You are Big Enough")}else{alert("You are small")}
 
